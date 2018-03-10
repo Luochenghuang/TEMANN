653ced8e9f2ea76e0d0d1e790b7b1e7dee133787
Ellen Murphy <emurphy89@uw.edu>
2018-03-09 19:45:03 -0800

    Added docstring.

--- temann/map_to_int.py
+++ temann/map_to_int.py
@@ -1,5 +1,7 @@
 def encode_columns(df, target_column):
-    """Change unique column values to integers.  df_mod2 needs to be """
+    """Change unique column values to integers.  target_column will
+	have values reassigned to integers inline. target_column needs to 
+	exsist within the feed df. """
     targets = df[target_column].unique()
     map_to_int = {name: n for n, name in enumerate(targets)}
     df[target_column] = df[target_column].replace(map_to_int)
