d9f2df930094aee1c2d3626a8ae92b286d090b36
Nathan Laurie <natelaur@uw.edu>
2018-03-13 15:43:01 -0700

    fixed test for expand_spacegroup (import problem), continuing test_plotting

--- temann/tests/test_plotting.py
+++ temann/tests/test_plotting.py
@@ -1,21 +1,21 @@
 # Unit test for plotting.py
-from ..plotting import *
+from ..plotting import three_elements_to_formula
 
 def test_three_elements_to_formula():
-    element1 = 1
-    element2 = 'Mn'
-    element3 = 'Sr'
-    stoich1 = 2
-    stoich2 = 3
-    stoich3 = 1
+    el1 = 1
+    el2 = 'Mn'
+    el3 = 'Sr'
+    st1 = 2
+    st2 = 3
+    st3 = 1
     try:
-        plotting.three_elements_to_formula(element1, element2, element3, stoich1, stoich2, stoich3)
+        three_elements_to_formula(el1, el2, el3, st1, st2, st3)
     except Exception:
         pass
     else:
-        raise Exception('Did not catch case of non-string element')
-    return True
+        raise Exception('Did not catch case of non-string input for element')
 
-    #(e1, e2, e3, n1, n2, n3):
-    #"""input 3 strs and 3 coefficients output the compound formula"""
-    #return e1 + str(n1) + e2 + str(n2) + e3 + str(n3)
+    el1 = 'Ca'
+    results = three_elements_to_formula(el1, el2, el3, st1, st2, st3)
+    assert results == 'Ca2Mn3Sr1'
+    return True
