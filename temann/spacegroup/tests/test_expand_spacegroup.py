0e3af80b8d454e618e6642fedc4928dea0832458
Luke Gibson <ldgibson@uw.edu>
2018-03-13 14:57:09 -0700

    fixed test so that tests are now passing

--- temann/spacegroup/tests/test_expand_spacegroup.py
+++ temann/spacegroup/tests/test_expand_spacegroup.py
@@ -1,4 +1,5 @@
-from .. import expand_spacegroup as sg
+#from .. import expand_spacegroup as sg
+from ..expand_spacegroup import expand_spacegroup
 #import ..expand_spacegroup as sg
 import numpy as np
 
@@ -11,25 +12,25 @@ def test_expand_spacegroup():
     a_list = [1, 2]
     # try all non-integer inputs
     try:
-        sg.expand_spacegroup(a_float)
+        expand_spacegroup(a_float)
     except Exception:
         pass
     else:
         raise Exception('Did not catch case of float input')
     try:
-        sg.expand_spacegroup(a_string)
+        expand_spacegroup(a_string)
     except(Exception):
         pass
     else:
         raise Exception('Did not catch case of string input')
     try:
-        sg.expand_spacegroup(a_list)
+        expand_spacegroup(a_list)
     except Exception:
         pass
     else:
         raise Exception('Did not catch case of list input')
     # check that output is correct length for any integer
     rand_int = np.random.randint(1, 230)
-    results = sg.expand_spacegroup(rand_int)
+    results = expand_spacegroup(rand_int)
     assert len(results) == 5
     return True
