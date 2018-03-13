f6993b3b9bfa9135d5e66f40b638ac5a13174d98
Luke Gibson <ldgibson@uw.edu>
2018-03-13 13:13:12 -0700

    pep8 compliance

--- temann/interpret/interpret.py
+++ temann/interpret/interpret.py
@@ -3,31 +3,33 @@ import re
 
 def get_empirical_formula(formula):
     """
-    Converts chemical formula to empirical formula and respective
-    coefficients for doping and proportions.
+    Parses a chemical formula into elements and proportions.
 
-    Input:
-    formula (string)
+    Converts chemical formula to a dictionary containing elements
+    and respective coefficients for doping and proportions.
+
+    Args:
+        formula (string): Chemical formula of a compound.
 
     Output:
-    elements (dictionary)
+        dictionary: Each key is a unique element in `formula` with the
+            values being the element's associated proportion.
 
     Example:
-    >>> import temann
-    >>> emp_form = temann.get_empirical_formula("Ca0.98La0.02MnO3")
-    >>> emp_form
-    {'Ca': 0.98, 'La': 0.02, 'Mn': 1, 'O': 3}
+        >>> from temann.interpret import get_empirical_formula
+        >>> get_empirical_formula("Ca0.98La0.02MnO3")
+        {'Ca': 0.98, 'La': 0.02, 'Mn': 1, 'O': 3}
     """
     assert not isinstance(formula, list), \
         "Cannot pass a list. Input must be a string"
-        
+
     assert isinstance(formula, str), "Must pass a string."
-    
+
     # Split formula into individual elements and their proportions
     proportions = re.findall("[A-Z][^A-Z]*", formula)
-    
+
     elements = {}
-    
+
     # Split each group into element and coefficient
     for pair in proportions:
         if bool(re.search(r"\d", pair)):
--- temann/interpret/tests/test_interpret.py
+++ temann/interpret/tests/test_interpret.py
@@ -2,11 +2,11 @@ from ..interpret import *
 
 
 def test_get_empirical_formula():
-    
+
     # Test formula inputs
     test_form1 = "Ca0.98La0.02MnO3"
     test_form2 = "LiO"
-    
+
     # Try passing a number
     try:
         get_empirical_formula(1234)
@@ -15,28 +15,27 @@ def test_get_empirical_formula():
     else:
         raise Exception("Bad input allowed",
                         "Error not raised when numerical value is passed")
-    
+
     # Check output of valid run
     elements1 = get_empirical_formula(test_form1)
-    
+
     assert set(['Ca', 'La', 'Mn', 'O']).issubset(elements1.keys()), \
         "Incorrect list of elements in formula"
-        
+
     assert set([0.98, 0.02, 1, 3]).issubset(elements1.values()), \
         "Incorrect list of coefficients in formula"
-        
+
     assert type(elements1['Ca']) is float, "Proportion value not a float"
     assert type(elements1['Mn']) is int, "Proportion value not an int"
     assert type(elements1['O']) is int, "Proportion value not an int"
-    
-       
+
     # Check output of valid run with no doping percentages
     elements2 = get_empirical_formula(test_form2)
-    
+
     assert set(['Li', 'O']).issubset(elements2.keys()), \
         "Incorrect list of elements in formula"
-        
+
     assert set([1, 1]).issubset(elements2.values()), \
         "Incorrect list of coefficients in formula"
-    
+
     return
