c3189855d0c5be6930faf3b1d976dc54388aae59
Luke Gibson <ldgibson@uw.edu>
2018-03-13 14:09:37 -0700

    removed python-ternary as a requirement

--- temann/version.py
+++ temann/version.py
@@ -76,4 +76,4 @@ MICRO = _version_micro
 VERSION = __version__
 PACKAGE_DATA = {'temann': [pjoin('data', '_data', '*')],
                 '': ['*.csv']}
-REQUIRES = ["numpy", "pandas", "ase", "pymatgen", "python-ternary"]
+REQUIRES = ["numpy", "pandas", "ase", "pymatgen"]
